4dd01bacaf0f38713d035de9d191bf95c235904e
lailaboris <liloboris05@gmail.com>
2024-04-30 12:59:17 +0200

    farve palette
    color

--- assets/js/site.js
+++ assets/js/site.js
@@ -20,10 +20,27 @@ const baseColors = [
 // denne funktion skal skabe dom elementer til styling så du kan få det resultat, din underviser hat vist dig.
 function createTiles(){
 
+  const myAppElement=document.getElementById("app");
+
+  const colorHeadline = document.createElement('h1');
+  colorHeadline.innerText = 'Color Palette';
+  myAppElement.appendChild(colorHeadline);
+
+
+
+  const myColorElement = document.createElement('section');
+  myColorElement.id="color"
+
+  myAppElement.appendChild(myColorElement);
+
 
 //du kan bruge denne array funktion til at loope gennem din array for at skabe dom elementer til hver farve.
  baseColors.forEach(color => {
-      // color er farve koderne fra baseColors som tekst strenge
+      const colorBox = document.createElement('div');
+      colorBox.classList.add('color-box');
+      colorBox.style.backgroundColor = color;
+      colorBox.textContent = color;
+      myColorElement.appendChild(colorBox);
       
       });
   }
